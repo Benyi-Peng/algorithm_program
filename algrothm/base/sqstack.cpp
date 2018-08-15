17bd3c4518c8df04db295b9fe2edd56b1ba3e9d2
彭本毅 <pengbenyi@oecore.com>
2018-08-15 18:38:16 +0800

    generic stack

--- algrothm/base/sqstack.cpp
+++ algrothm/base/sqstack.cpp
@@ -9,7 +9,7 @@
 #include "sqstack.h"
 
 #include <stdlib.h>
-
+#include <string.h>
 
 void InitStack(SqStack *&s) {
     s=(SqStack *)malloc(sizeof(SqStack));
@@ -45,4 +45,50 @@ bool GetTop(SqStack *s,ElemType &e) {
         return false;
     e=s->data[s->top];
     return true;
+}
+
+
+
+
+
+
+
+
+typedef struct {
+    void *elems;    //栈底指针
+    int loglen;       //已经使用的长度
+    int alloclen;    //已经申请的长度
+    int elemSize;   //单个元素的长度
+} stack;
+
+static void StackGrow(stack *s);
+
+void StackNew(stack *s, int elemSize) {
+    s->elemSize = elemSize;
+    s->loglen = 0;
+    s->alloclen = 4;
+    s->elems = malloc(4 *elemSize);
+}
+
+void StackPush(stack *s, void *elemAdd) {
+    //栈满则采用加倍策略增长
+    if (s->loglen == s->alloclen) {
+        StackGrow(s);
+    }
+    
+    void *target = (char *)(s->elemSize) + s->loglen * (s->elemSize);
+    memcpy(target, elemAdd, s->elemSize);
+    s->loglen++;
+}
+
+static void StackGrow(stack *s) {
+    s->alloclen *= 2;
+    s->elems = realloc(s->elems, (s->alloclen) * (s->elemSize));
+}
+
+void StackPop(stack *s,  void *elempop) {
+    //获取栈顶元素指针
+    void *source = (char*)(s->elems) + (s->loglen - 1 ) *(s->elemSize);
+    memcpy(elempop, source, s->elemSize);
+    s->loglen--;
 }
